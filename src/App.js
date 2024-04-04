a10fa3693c3fc314cf8179675b4c08bfdc7d9d35
Amit manore <96157492+Amyth0007@users.noreply.github.com>
2024-04-04 12:56:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css'; // Import CSS file for styling
 
 const App = () => {
   // Retrieve data from local storage or use default values
-  const initialNormalRooms = parseInt(localStorage.getItem('normalRooms'), 10) || 50;
-  const initialOxygenRooms = parseInt(localStorage.getItem('oxygenRooms'), 10) || 50;
-  const initialICURooms = parseInt(localStorage.getItem('ICURooms'), 10) || 20;
-  const initialFlatBeds = parseInt(localStorage.getItem('FlatBeds'), 10) || 80;
-  const initialReclinerBeds = parseInt(localStorage.getItem('reclinerBeds'), 10) || 100;
-  const initialVentilators = parseInt(localStorage.getItem('ventilators'), 10) || 20;
-  const initialOxygenCylinders = parseInt(localStorage.getItem('oxygenCylinders'), 10) || 110;
-  const initialNonRebreatherMasks = parseInt(localStorage.getItem('nonRebreatherMasks'), 10) || 200;
-  const initialNormalMasks = parseInt(localStorage.getItem('normalMasks'), 10) || 120;
+  const initialNormalRooms = parseInt(localStorage.getItem('normalRooms')) || 30;
+  const initialOxygenRooms = parseInt(localStorage.getItem('oxygenRooms')) || 50;
+  const initialICURooms = parseInt(localStorage.getItem('ICURooms')) || 20;
+  const initialFlatBeds = parseInt(localStorage.getItem('FlatBeds')) || 80;
+  const initialReclinerBeds = parseInt(localStorage.getItem('reclinerBeds')) || 100;
+  const initialVentilators = parseInt(localStorage.getItem('ventilators')) || 16;
+  const initialOxygenCylinders = parseInt(localStorage.getItem('oxygenCylinders')) || 110;
+  const initialNonRebreatherMasks = parseInt(localStorage.getItem('nonRebreatherMasks')) || 200;
+  const initialNormalMasks = parseInt(localStorage.getItem('normalMasks')) || 120;
 
   // simillary we have to keep record  of flat beds availibility is 80, then recliner beds 100, ventilator 20, oxygen cylinder 110, non rebreather masks 200, normal masks 120
   const [FlatBeds, setFlatBeds] = useState(initialFlatBeds);
@@ -29,17 +29,17 @@ const App = () => {
 
   // Save data to local storage whenever room counts change
   useEffect(() => {
-    localStorage.setItem('normalRooms', normalRooms);
-    localStorage.setItem('oxygenRooms', oxygenRooms);
-    localStorage.setItem('ICURooms', ICURooms);
-    localStorage.setItem('FlatBeds', FlatBeds);
-    localStorage.setItem('reclinerBeds', reclinerBeds);
-    localStorage.setItem('ventilators', ventilators);
-    localStorage.setItem('oxygenCylinders', oxygenCylinders);
-    localStorage.setItem('nonRebreatherMasks', nonRebreatherMasks);
-    localStorage.setItem('normalMasks', normalMasks);
+    localStorage.setItem('normalRooms', initialNormalRooms);
+    localStorage.setItem('oxygenRooms', initialOxygenRooms);
+    localStorage.setItem('ICURooms', initialICURooms);
+    localStorage.setItem('FlatBeds',  initialFlatBeds);
+    localStorage.setItem('reclinerBeds', initialReclinerBeds);
+    localStorage.setItem('ventilators', initialVentilators);
+    localStorage.setItem('oxygenCylinders', initialOxygenCylinders);
+    localStorage.setItem('nonRebreatherMasks', initialNonRebreatherMasks);
+    localStorage.setItem('normalMasks', initialNormalMasks);
 
-  }, [normalRooms, oxygenRooms, ICURooms, FlatBeds, reclinerBeds, ventilators, oxygenCylinders, nonRebreatherMasks, normalMasks]);
+  }, [initialNormalRooms, initialOxygenRooms, initialICURooms, initialFlatBeds, initialReclinerBeds, initialVentilators, initialOxygenCylinders, initialNonRebreatherMasks, initialNormalMasks]);
 
   const reserveRoom = (roomType) => {
     if (roomType === 'ICU') {
@@ -48,16 +48,16 @@ const App = () => {
         setVentilators(ventilators - 1);
         setOxygenCylinders(oxygenCylinders - 1);
         setReclinerBeds(reclinerBeds - 1);
-        if(ventilators>0 || oxygenCylinders>0 || reclinerBeds>0){
+        if(ventilators > 0 || oxygenCylinders > 0 || reclinerBeds > 0){
         setReservationMessage(`01 ICU room (with 1 ventilator + 1 oxygen cylinder + 1 recliner bed) reserved.`);
         }else{
-          if(ventilators===0){
+          if(ventilators <= 0){
             alert("ventilators are not available so room cant be reserved")
             setReservationMessage('Sorry, no ICU rooms could be reserved.');
-          }else if(oxygenCylinders===0){
+          }else if(oxygenCylinders<=0){
             alert("oxygen cylinders are not available so room cant be reserved")
             setReservationMessage('Sorry, no ICU rooms could be reserved.');
-          }else if(reclinerBeds===0){
+          }else if(reclinerBeds<=0){
             alert("recliner beds are not available so room cant be reserved")
             setReservationMessage('Sorry, no ICU rooms could be reserved.');
           }
@@ -74,13 +74,13 @@ const App = () => {
         if(FlatBeds>0 || oxygenCylinders>0 || nonRebreatherMasks>0){
         setReservationMessage(`01 Oxygen room (2 oxygen cylinder + 1 recliner bed + 2 non rebreather masks) reserved.`);
         }else{
-          if(FlatBeds===0){
+          if(FlatBeds<=0){
             alert("flat beds are not available so room cant be reserved")
             setReservationMessage('Sorry, no ICU rooms could be reserved.');
-          }else if(oxygenCylinders===0){
+          }else if(oxygenCylinders<=0){
             alert("oxygen cylinders are not available so room cant be reserved")
             setReservationMessage('Sorry, no ICU rooms could be reserved.');
-          }else if(nonRebreatherMasks===0){
+          }else if(nonRebreatherMasks<=0){
             alert("non rebreather masks are not available so room cant be reserved")
             setReservationMessage('Sorry, no ICU rooms could be reserved.');
           }
@@ -98,11 +98,11 @@ const App = () => {
 
           setReservationMessage(`01 Normal room (1 normal bed + 2 normal masks ) reserved.`);
         }else{
-          if(FlatBeds===0){
+          if(FlatBeds<=0){
 
             alert("flat beds are not available so room cant be reserved")
             setReservationMessage('Sorry, no ICU rooms could be reserved.');
-          }else if(normalMasks===0){
+          }else if(normalMasks<=0){
             alert("normal masks are not available so room cant be reserved")
             setReservationMessage('Sorry, no ICU rooms could be reserved.');
           }
@@ -128,7 +128,7 @@ const App = () => {
 
       </div>
       <div className="equipment-status">
-        <p>Normal Beds: {FlatBeds ? FlatBeds : "flatbeds are not available so room cant be reserved"}</p>
+        <p>Normal Beds: {FlatBeds}</p>
         <p>Recliner Beds: {reclinerBeds}</p>
         <p>Ventilators: {ventilators}</p>
         <p>Oxygen Cylinders: {oxygenCylinders}</p>
